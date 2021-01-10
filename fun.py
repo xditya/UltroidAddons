6e882c60a3424fa5a619c26fa0765234589b2050
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-01-10 22:10:05 +0530

    Update fun.py

--- fun.py
+++ fun.py
@@ -6,17 +6,15 @@ import json
 
 @ultroid_cmd(pattern="joke")
 async def _(ult): 
-    await ult.edit(get_joke()) 
+    await eor(ult, get_joke()) 
 
 @ultroid_cmd(pattern="url ?(.*)")
 async def _(event):
-    if event.fwd_from:
-        return
     input_str = event.pattern_match.group(1)
     sample_url = "https://da.gd/s?url={}".format(input_str)
     response_api = requests.get(sample_url).text
     if response_api:
-        await event.edit("Shortened url==> {} for the given url==> {}.".format(response_api, input_str))
+        await eor(event, "Shortened url==> {} for the given url==> {}.".format(response_api, input_str))
     else:
-        await event.edit("something w3nt wrong. please try again later.")
+        await eor(event, "something w3nt wrong. please try again later.")
 
