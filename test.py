0851f6504a74c4f68b23aba47c207b96460c3fa1
Danish <72792730+1Danish-00@users.noreply.github.com>
2021-02-02 17:34:03 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 # Ported From DarkCobra Originally By UNIBORG
+#
+# Ultroid - UserBot
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
 from telethon import events
 from datetime import datetime
