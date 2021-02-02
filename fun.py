d80fbcc03643cec433f984eb2f35c146d4aa5458
hellboi_atul <68107352+hellboi-atul@users.noreply.github.com>
2021-02-02 18:45:55 +0530

    Update fun.py

--- fun.py
+++ fun.py
@@ -9,10 +9,15 @@
     
 • `{i}decide`
     Decide something.
+
+• `{i}xoxo`
+    Opens tic tac toe game only where using inline mode is allowed.
+
+
     
 """
 
-import os
+import os, re, random
 from pyjokes import get_joke
 import requests
 import json
@@ -44,5 +49,39 @@ async def _(event):
         event.chat_id, r["answer"], reply_to=message_id, file=r["image"])
     except ChatSendMediaForbiddenError:
         await eor(event,r['answer'])
+
+UNSAFE_CHARACTERS = re.compile(
+    "["
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats 
+    "]+")
+def deEmojify(inputString: str) -> str:
+    # Remove emojis and other unsafe characters from string
+    return re.sub(UNSAFE_CHARACTERS, '', inputString)
+@ultroid_cmd(pattern="xoxo")
+async def nope(doit):
+    ok = doit.pattern_match.group(1)
+    if not ok:
+        if doit.is_reply:
+            wtf = (await doit.get_reply_message()).message
+        
+            return
+    xoxoxo = await bot.inline_query(
+        "xobot", f"{(deEmojify(ok))}")
+    await xoxoxo[0].click(doit.chat_id,
+                            reply_to=doit.reply_to_msg_id,
+                            silent=True if doit.is_reply else False,
+                            hide_via=True)
+    await doit.delete()
+
     
 HELP.update({f"{__name__.split('.')[2]}": f"{__doc__.format(i=Var.HNDLR)}"})
